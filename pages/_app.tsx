3c4d73ba5e6c36812ff07c6714b70b8b87497ffc
k.tamori@digisaku.co.jp <k.tamori@digisaku.co.jp>
2022-12-04 23:42:35 +0900

    sample

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Noto_Sans_JP } from "@next/font/google";
+
+const notoSans = Noto_Sans_JP({
+  weight: ['400', '500', '700'],
+  style: ['normal'],
+  subsets: ['japanese']
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return ( 
+    <main className={notoSans.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
